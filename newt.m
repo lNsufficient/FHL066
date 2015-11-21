55e26b13cf41d1e7054810a68a8ec6eda7390d59
Edvard <edvard.jo@gmail.com>
2015-11-21 10:44:39 +0100

    Full newton rapson working

--- newt.m
+++ newt.m
@@ -31,16 +31,12 @@ for n = 1:nbr_steps
     
     res=TOL+1;
     
-    if n==2
-        keyboard;
-    end
-    
     count=0;
     
     while res > TOL
         count = count+1;
         r = f;
-        K=K*0;
+        %K=K*0;
         %r =  zeros(ndof,1);
         for j = 1:nelm
             index_dof=Edof(j,2:end); %de frihg stången gränsar till
@@ -50,14 +46,14 @@ for n = 1:nbr_steps
             [es, ee] = bar3gs(ec,ep,ed);
             ef=bar3gf(ec,ed,es);
             r(index_dof) = r(index_dof) - ef;
-        Ke = bar3ge(ec,ep,ed,es);
-        K(index_dof,index_dof)=K(index_dof,index_dof)+Ke;
+        %Ke = bar3ge(ec,ep,ed,es);
+        %K(index_dof,index_dof)=K(index_dof,index_dof)+Ke;
         end
         da = solveq(K, r, bc);
         a=a+da;
         r(bc(:,1))=0;
         res = norm(r);
-        [res,norm(da)]
+        %[res,norm(da)]
         for k=1:3
             coord(:,k) = coord0(:,k)+a(k:3:(end+k-3));
         end
@@ -65,6 +61,9 @@ for n = 1:nbr_steps
     plot_f(n) = f(top_dof);
     plot_a(n) = a(top_dof);
 end
-plot(abs(plot_a), abs(plot_f))
-xlabel('förskjutning / meter')
+
+%%
+%plot(abs(plot_a), abs(plot_f))
+plot(plot_a, plot_f)
+xlabel('f�rskjutning / meter')
 ylabel('kraft / Newton')
